c2ef13f3941ab2a855e00fff64ba8532c46b1326
Wyatt DeRamus <deramus.wyatt@gmail.com>
2022-11-09 12:07:09 -0600

    difuse the BOM

--- js/warmups.js
+++ js/warmups.js
@@ -1,36 +1,21 @@
+
 // Write a function that when passed an object will return the value of the object’s price property.
 //     Ex. let obj = {name: “Potatoes”, quantity: 2, price: “$7.89"};
 // getPrice(obj); // returns “$7.89”
 
-function getPrice(obj){
-    return obj.price
-
-}
-let oranges ={
-    name: "oranges",
-    quantity: 2,
-    price: "7.89"
-
-}
-console.log(getPrice(oranges));
+// function getPrice(obj){
+//     return obj.price
+//
+// }
+// let oranges ={
+//     name: "oranges",
+//     quantity: 2,
+//     price: "7.79"
+//
+// }
+// console.log(getPrice(oranges));
 
 // Write a function that when passed an array will return the longest string from the array.
-//     let colors = [“Green”, “Blue”, “Periwinkle”];
-// getLongestString(arr) // Returns “Periwinkle”
-// function getLongestString(arr){
-//  let longestStr = " ";
-//  colors.forEach(function(color){
-//  if(color.length > longestStr.length) {
-//      longestStr = color;
-//  }
-//  });
-//    return longestStr;
-// }
-// console.log(getLongestString(colors));
-
-// function getLongestStr(arr){
-//     for
-// }
 
 function getLongestString(arr){
     let longestStr = "";
@@ -180,7 +165,7 @@ const hamsters = [
 
 // getAverageHeight(hamsters); // should return 93.2;
 
-function getAverageHeight(arr) {
+// function getAverageHeight(arr) {
 //     let totalHeight = 0;
 //     for (let element of arr) {
 //         totalHeight += element.heightInMM
@@ -188,7 +173,7 @@ function getAverageHeight(arr) {
 //     return totalHeight / arr.length;
 // }
 
-console.log(getAverageHeight(hamsters));
+// console.log(getAverageHeight(hamsters));
 for(let i = 0; i < grades.length; i++) {
         total += grades[i];
     }
@@ -208,16 +193,38 @@ for(let i = 0; i < grades.length; i++) {
 //
 //     ```
 // ex.
-// let potatoes = {
-// 	name: potatoes,
-// 	quantity: 20,
-// 	totalPrice: 47
-// };
+let potatoes = {
+	name: "potatoes",
+	quantity: 20,
+	totalPrice: 47
+};
 //
 // getPricePerItem(potatoes) // returns "$2.35"
 // ```
 //
-// 2. Write a function that takes in two string arguments: the first being the string assessed, and the second being a single letter. The function should return a count of the occurrences of the letter (second argument) within the string (first argument). This function should count both upper and lowercase instances of the letter.
+
+function getPricePerItem(obj){
+    return obj.totalPrice;
+
+}
+
+console.log(getPricePerItem(potatoes));
+
+function getPrice(obj){
+    return obj.price
+
+}
+
+// 2. Write a function that takes in two string arguments:
+//
+// the first being the string assessed,
+// and the second being a single letter.
+//
+// The function should return a count of the occurrences of the letter (second argument) within the string (first argument).
+//
+// This function should count both upper and lowercase instances of the letter.
+//
+//
 //
 //     ```
 // ex.
@@ -225,33 +232,77 @@ for(let i = 0; i < grades.length; i++) {
 // countOccurrencesOfLetter("Microsoft Excel: Professional Edition". "E") // returns 4
 // countOccurrencesOfLetter("Sanrio", "x") // returns 0
 // ```
+
+let count = 0;
+function countOccurrencesOfLetter(strAss, singLet){
+    let letter_Count = 0;
+    for (var position = 0; position < str.length; position++)
+    {
+        if (str.charAt(position) == letter)
+        {
+            letter_Count += 1;
+        }
+    }
+    return letter_Count;
+}
+}
+
+function countOccurrencesOfLetter(word, letter) {
+    let number = 0
+    for (i=0; i < word.length; i++) {
+        if (word.substring(i, i+1) == letter.toUpperCase() || word.substring(i, i+1) == letter.toLowerCase()){
+            number++;
+        }
+    }
+    return number
+}
+
 //
 // 3. Write a function that takes in an array of  grocery item objects and returns the object with the highest `weightInGrams` property.
 //
 //     ```
 // ex.
-// const cart = [
-// 	{
-// 		name: "Kingsford Charcoal",
-// 		weightInGrams: 4535
-// 	}, {
-// 		name: "Krazy Glue",
-// 		weightInGrams: 5
-// 	}, {
-// 		name: "NVIDIA RTX 4090",
-// 		weightInGrams: 2186
-// 	}
-// ];
+const cart = [
+	{
+		name: "Kingsford Charcoal",
+		weightInGrams: 4535
+	}, {
+		name: "Krazy Glue",
+		weightInGrams: 5
+	}, {
+		name: "NVIDIA RTX 4090",
+		weightInGrams: 2186
+	}
+];
 // getHeaviestItem(cart) // returns {name: "Kingsford Charcoal", weightInGrams: 4535}
 // ```
+function getHeaviestItem(x){
+    let heaviest = {weightInGrams: 0}
+    for (i = 0; i < x.length; i++){
+        if(x[i].weightInGrams > heaviest.weightInGrams){
+            heaviest = x[i];
+        }
+    }
+    return heaviest;
+}
+
+console.log(getHeaviestItem(cart))
 //
 // 4. Write a function that takes in an array of numbers and returns an array of numbers from the original array that are evenly divisible by 13. If a number is not evenly divisible by 13, it should not be returned. If no elements within the array are evenly divisible by 13, the function should return an empty array.
 //
+function getAllDivisibleByThirteen(arr){
+    let divide = [];
+    for (i=0; i < arr.length; i++){
+        if (arr[i] % 13 === 0)
+            divide.push(arr[i]);
+    }
+    return divide;
+}
 //     ```
 // ex.
-// const arr1 = [24, 25, 26, 27, 29];
-// const arr2 = [13, 26, 39, 52, 65];
-// const arr3 = [0, 1, 2, 3, 4, 5];
+const arr1 = [24, 25, 26, 27, 29];
+const arr2 = [13, 26, 39, 52, 65];
+const arr3 = [0, 1, 2, 3, 4, 5];
 //
 // getAllDivisibleByThirteen(arr1) // returns [26]
 // getAllDivisibleByThirteen(arr2) // returns [13, 26, 39, 52, 65]
@@ -263,14 +314,184 @@ for(let i = 0; i < grades.length; i++) {
 // 2. `numberOfWords` which is to be set to the number of words within the string.
 // 3. `instancesOfE` which to be set to the number of instances of the letter "E" within the argument passed into the function. The count should reflect instances of both uppercase and lowercase "E" (HINT: a previous function maybe useful in achieving this).
 //
+function reviewString(string) {
+    let newstring = {string: "",
+        numberOfWords: 1,
+        instancesOfE: 0};
+    newstring.string = string;
+    for (i=0; i < string.length; i++) {
+        if (string.substring(i, i+1) == "e" || string.substring(i, i+1) == "E"){
+            newstring.instancesOfE++
+        }
+        if (string.substring(i, i+1) == " "){
+            newstring.numberOfWords++
+        }
+    }
+    return newstring;
+}
 // ```
 // ex.
 // reviewString("Dielectric Grease") // returns {string: "Dielectric Grease", numberOfWords: 2, instancesOfE: 3}
 // reviewString("Street Fighter EX3") // returns {string: "Street Fighter EX3", numberOfWords: 3, instancesOfE: 4}
 // reviewString("99") // returns {string: "99", numberOfWords: 1, instancesOfE: 0}
 // ```
+function isPositive(num){
+    if(num > 0){
+     return true;
+    }else{
+       return false;
+    }
+}
+
+console.log(9);
+
+function isFortyTwo(){
+    if(num === 42){
+      return true;
+    }else{
+      return false;
+    }
+}
+let numArr = [1, 2, 3, 4,]
+let numArr2 = [5, 6, 7, 8, 9,]
+function countEvans(arr) {
+    let count = 0;
+    arr.forEach(function (element) {
+        if (element % 2 === 0) {
+            count++
+        }
+    });
+    return count;
+}
+
+function isAllLowerCase (str){
+    if(str === string.toLowerCase()){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+function returnObject(string){
+    let newObj = {};
+    newObj.string = string;
+    newObj.numberOfWords = countLetters(string, " ")
+    newObj.eCount = countLetters(string, "E")
+    return newObj;
+}
 
 
+
+function countLetters(stringOne, letter) {
+    let myString = stringOne.toLowerCase();
+    let mySubString = letter.toLowerCase();
+    let count = myString.split(mySubString).length - 1;
+    console.log(count);
+    return count;
+}
+
+
+// walk through 2nd assessment//////////////////////////////////
+// Write a function named isNegative that accepts a number and returns true or false based on whether the input is negative.
+// function isNegative(arg){
+    return arg < 0;
+}
+//Write a function named isTen that accepts a number and returns a boolean that indicates whether or not that number is equal to 10.
+function isTen(num){
+    return num === 10;
+}
+//Write a function named double that accepts a number and returns the number doubled.
+function double(num){
+    return num * 2;
+}
+//Write a function named remove9s that accepts an array of numbers and returns an array with all the same numbers except for 9.
+function remove9s(arr){
+    let finalArr = [];
+    for(let num of arr){
+        if(num !== 9){
+            finalArr.push(num);
+        }
+    }
+    return finalArr;
+}
+//Write a function named average that accepts an array of numbers and returns the average of those numbers.
+function average(arr){
+    let total = 0;
+    for(let num of arr){
+        total += num;
+    }
+    return total / arr.length;
+}
+//Write a function named countOdds that accepts an array of numbers and returns the number of odd numbers in the array.
+function countOdds(arr){
+    let count = 0;
+    for(let num of arr){
+        if(num % 2 !== 0){
+            count++;
+        }
+    }
+    return count;
+}
+//Write a function named averageSales that accepts an array of objects where each object represents a person, and has a sales property. The function should return the average of every object's sales property.
+function averageSales(arr){
+    let total = 0;
+    for(let person of arr){
+        total += person.sales;
+    }
+    return total / arr.length;
+}
+//Write a function named convertNameToObject that accepts a string that contains a first name and last name separated by a space character, and returns an object with properties firstName and lastName.
+function convertNameToObject(name){
+    let splitName = name.split(" ");
+    return{
+        firstName: splitName[0],
+        lastName: splitName[1]
+    };
+}
+//Write a function named countVowels that accepts a string and returns the number of vowels in that string. (Don't worry about or count "y" as a vowel)
+function countVowels(str){
+    const vowels = ["a","e","i","o","u"];
+    const splitString = str.toLowerCase().split("");
+    let count = 0;
+    for(let char of splitString){
+        if(vowels.includes(char)){
+            count++;
+        }
+    }
+    return count;
+}
+// function countVowels(str) {
+//     let count = 0;
+//     let splitStr = str.split('');
+//     for (let i = 0; i < splitStr.length; i++) {
+//         if (isVowel(splitStr[i])) {
+//             count++
+//         }
+//     }
+//     return count
+// }
+//Write a function named analyzeWord. It should take in a string and return an object with information about the input word. The object returned should have the following properties:
+//
+// word: the original word that was passed into the function
+// numberOfLetters: the number of letters in the word
+// numberOfVowels: the number of vowels in the word
+function analyzeWord(word){
+    return{
+        word: word,
+        numberOfletters: word.length,
+        numberOfVowels: countVowels(word)
+    };
+}
+//Write a function named capitalizeName that accepts a string that is a first and last name separated by a space, and returns a string that that has the first and last names capitalized.
+//
+// For this problem, you may assume that the function will only ever be called with a string that has two words separated by a single space.
+function capitalizeName(name){
+    let splitName = name.split(" ");
+    for(let i = 0; i < splitName.length; i++){
+        splitName[i] = splitName[i].substring(0,1).toUpperCase() + splitName[i].substring(1).toLowerCase()
+    }
+    return splitName.join(" ");
+}
 
 
 
